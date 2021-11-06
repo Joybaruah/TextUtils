f4ac8ba7d9b0c6d4f13e142b708ed4562b19f4a8
Joybaruah <hoyyun1@gmail.com>
2021-11-07 01:12:01 +0530

    Day 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 
 function App() {
   return (
   <>
   <Navbar title="TextUtils"/>
+  <div className="container my-4">
+    <TextForm heading="Enter your text to analyze"/>
+  </div>
 </>
   )
 };
